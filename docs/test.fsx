c3993a3be5b8509478709ab2f4935e7a61599510
MalteLueder <98827790+MalteLueder@users.noreply.github.com>
2022-03-03 09:10:03 +0000

    adding a graph

---
+++ docs/test.fsx
@@ -0,0 +1,27 @@
+//this is my page
+//(***include.ld)
+
+#load "YahooFinance.fsx"
+
+open YahooFinance
+
+let twnk = YahooFinance.PriceHistory("TWNK")
+
+twnk.[..3]
+
+#r "nuget: Plotly.NET, 2.0.0-preview.17"
+
+open Plotly.NET 
+
+
+twnk
+|> List.map (fun day -> day.Date, day.AdjustedClose)
+|> Chart.Line
+|> GenericChart.toChartHTML
+(***include-it-raw***)
+
+
+
+
+
+
